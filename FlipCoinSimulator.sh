d1353a6268af41e255d2d8f63649424e321174c3
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-06 22:42:00 +0530

    updated Tie

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -19,26 +19,16 @@ fi
 #loop For Simulator
 while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
 do
-	toss=$((RANDOM%2))
-	if [ $toss -eq 1 ]
-	then
-		echo "HEAD"
-		((hwin++))
-	else
-		echo "TAILS"
-		((twin++))
-	fi
-	flip
+    flip
 done
 echo "NO OF TIME HEAD WIN"$hwin
 echo "NO OF TIME TAIL WIN"$twin
 
 if [ $hwin -eq $twin ]
 then
-echo "HEAD WIN BY"$((hwin-twin))
-flip
+	flip
 else
-echo "TAILS WIN BY"$((hwin-twin))
+
 if [ $hwin -gt $twin ]
 	then
 		echo "head won by"$((hwin-twin))
