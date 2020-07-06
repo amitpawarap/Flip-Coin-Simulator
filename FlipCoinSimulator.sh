6b263b2ef26d2d0daaaf2c5a84cd9f82b6dd2591
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-06 21:56:41 +0530

    Coin flip loop for simulator

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -9,4 +9,22 @@ else
 	echo "loss"
 fi
 #...............................................................................................
+#loop For Simulator
+hwin=0
+twin=0
+for((i=0;i<10;i++))
+do
+	toss=$((RANDOM%2))
+	if [ $toss -eq 1 ]
+	then
+		echo "HEAD"
+		((hwin++))
+	else
+		echo "TAILS"
+		((twin++))
+	fi
+done
+echo "NO OF TIME HEAD WIN"$hwin
+echo "NO OF TIME TAIL WIN"$twin
 
+#.......................................................................................................
